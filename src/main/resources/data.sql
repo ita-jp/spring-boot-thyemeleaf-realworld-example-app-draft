79588bf7da7e004be5f8e0f1a92cf059d80ee700
ita-jp <tn00gm@gmail.com>
2022-07-07 18:33:57 +0900

    data.sql (article)

---
+++ src/main/resources/data.sql
@@ -0,0 +1,2 @@
+INSERT INTO articles (id, author_id, title, description, body) VALUES ('4d8e67c8-fc6f-447c-ba6e-65fb9f56157c', '094f9443-93b0-4edb-b2a4-f85be7caa912', 'This is title.', 'This is description.', 'This is body.');
+INSERT INTO articles (id, author_id, title, description, body) VALUES ('11590afa-a379-49c6-8723-5211f774fb84', '094f9443-93b0-4edb-b2a4-f85be7caa912', 'This is title.', 'This is description.', 'This is body.');
