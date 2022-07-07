6067301e52555419e700359739700fbf56c954ff
ita-jp <tn00gm@gmail.com>
2022-07-07 12:48:21 +0900

    spring initializr (dependency = web only)

---
+++ src/test/java/com/pocotech/conduit/ConduitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pocotech.conduit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConduitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
