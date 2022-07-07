0b7262debe4b1fddf787d6ec096dd6d81e0ec37b
ita-jp <tn00gm@gmail.com>
2022-07-07 14:13:30 +0900

    di

--- src/main/java/com/pocotech/conduit/controller/IndexController.java
+++ src/main/java/com/pocotech/conduit/controller/IndexController.java
@@ -8,7 +8,11 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class IndexController {
 
-    private ArticleService articleService = new ArticleService();
+    private final ArticleService articleService;
+
+    public IndexController(ArticleService articleService) {
+        this.articleService = articleService;
+    }
 
     @GetMapping
     public String index(Model model) {
--- src/main/java/com/pocotech/conduit/service/article/ArticleService.java
+++ src/main/java/com/pocotech/conduit/service/article/ArticleService.java
@@ -1,8 +1,11 @@
 package com.pocotech.conduit.service.article;
 
+import org.springframework.stereotype.Service;
+
 import java.time.LocalDate;
 import java.util.List;
 
+@Service
 public class ArticleService {
 
     public List<ArticlePreviewEntity> list() {
